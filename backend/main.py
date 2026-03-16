fab9e9ea3f8e1910b72abee39bab10e413c40a01
Seun Samuel-Ipaye <seuneipaye@gmail.com>
2026-03-16 18:33:06 -0400

    parties done for now

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,12 @@
 from pydantic import BaseModel
-
-from fastapi import FastAPI
+import string
+import random
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 party_list = []
 
-import string
-import random
+
 
 def generate_random_code(size, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choices(chars, k=size))
@@ -19,21 +19,35 @@ class PartyCreate(BaseModel):
 def read_root():
     return {"message": "Play This backend is running"}
 
-@app.post("/parties/{title}")
+@app.post("/parties")
 def create_party(party_data: PartyCreate):
     code = generate_random_code(6)
     party_id = generate_random_code(10)
     
-    
-    party_list.append({
+    party = {
         "title": party_data.title,
         "id": party_id,
         "code": code,
-    })
-    
-    return {
-        "title": party_data.title,
-        "id": party_id,
-        "code": code,
-        
     }
+    
+    party_list.append(party)
+    
+    return party
+
+@app.get("/parties")
+def get_parties():
+    return party_list
+
+@app.get("/parties/{party_id}")
+def get_party_from_id(party_id: str):
+    for party in party_list:
+        if party["id"] == party_id:
+            return party
+    raise HTTPException(status_code=404, detail="Party not found")
+
+@app.get("/parties/code/{party_code}")
+def get_party_from_code(party_code: str):
+    for party in party_list:
+        if party["code"] == party_code:
+            return party
+    raise HTTPException(status_code=404, detail="Party not found")
