4e826a7b1be930ac83b98019369eb73a5ecfed18
Seun Samuel-Ipaye <seuneipaye@gmail.com>
2026-03-18 16:36:56 -0400

    adding guests works

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,19 @@ import random
 from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
-party_list = []
+party_list = [
+  {
+    "title": "back to school",
+    "id": "YPW8UI7GXJ",
+    "code": "XXPLTS"
+  },
+  {
+    "title": "insomnia",
+    "id": "24BCGYBL8C",
+    "code": "ABUMJM"
+  }
+]
+guest_list = []
 
 
 
@@ -14,11 +26,15 @@ def generate_random_code(size, chars=string.ascii_uppercase + string.digits):
 class PartyCreate(BaseModel):
     title: str
 
+class GuestJoin(BaseModel):
+    name: str
+    party_code: str
 
 @app.get("/")
 def read_root():
     return {"message": "Play This backend is running"}
 
+#party
 @app.post("/parties")
 def create_party(party_data: PartyCreate):
     code = generate_random_code(6)
@@ -51,3 +67,24 @@ def get_party_from_code(party_code: str):
         if party["code"] == party_code:
             return party
     raise HTTPException(status_code=404, detail="Party not found")
+
+#guest
+@app.post("/guests/join")
+def guest_join(guest_data: GuestJoin):
+    
+    for party in party_list:
+        if guest_data.party_code == party["code"]:
+            guest_id = generate_random_code(6)
+            
+            guest = {
+                "name": guest_data.name,
+                "guest_id": guest_id,
+                "party_id": party["id"]
+                }
+            
+            guest_list.append(guest)
+            return guest
+            
+        
+    raise HTTPException(status_code=404, detail="Party not found")
+        
