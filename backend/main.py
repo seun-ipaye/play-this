9174b1d18673852909f9469dc80e4b3e6bc99df9
Seun Samuel-Ipaye <seuneipaye@gmail.com>
2026-03-18 20:55:53 -0400

    view guests

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ import random
 from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
-party_list = [
+room_list = [
   {
     "title": "back to school",
     "id": "YPW8UI7GXJ",
@@ -16,75 +16,105 @@ party_list = [
     "code": "ABUMJM"
   }
 ]
-guest_list = []
+guest_list = [
+  {
+    "name": "seun",
+    "guest_id": "0JCSUZ",
+    "room_id": "YPW8UI7GXJ"
+  },
+  {
+    "name": "kamso",
+    "guest_id": "VWIMCY",
+    "room_id": "YPW8UI7GXJ"
+  },
+  {
+    "name": "deji",
+    "guest_id": "JAQN6D",
+    "room_id": "YPW8UI7GXJ"
+  }
+]
 
 
 
 def generate_random_code(size, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choices(chars, k=size))
 
-class PartyCreate(BaseModel):
+class RoomCreate(BaseModel):
     title: str
 
 class GuestJoin(BaseModel):
     name: str
-    party_code: str
+    room_code: str
 
 @app.get("/")
 def read_root():
     return {"message": "Play This backend is running"}
 
-#party
-@app.post("/parties")
-def create_party(party_data: PartyCreate):
+#room
+@app.post("/rooms")
+def create_room(room_data: RoomCreate):
     code = generate_random_code(6)
-    party_id = generate_random_code(10)
+    room_id = generate_random_code(10)
     
-    party = {
-        "title": party_data.title,
-        "id": party_id,
+    room = {
+        "title": room_data.title,
+        "id": room_id,
         "code": code,
+        "guest_list": []
     }
     
-    party_list.append(party)
+    room_list.append(room)
     
-    return party
+    return room
 
-@app.get("/parties")
-def get_parties():
-    return party_list
+@app.get("/rooms")
+def get_rooms():
+    return room_list
 
-@app.get("/parties/{party_id}")
-def get_party_from_id(party_id: str):
-    for party in party_list:
-        if party["id"] == party_id:
-            return party
-    raise HTTPException(status_code=404, detail="Party not found")
+@app.get("/rooms/{room_id}")
+def get_room_from_id(room_id: str):
+    for room in room_list:
+        if room["id"] == room_id:
+            return room
+    raise HTTPException(status_code=404, detail="Room not found")
 
-@app.get("/parties/code/{party_code}")
-def get_party_from_code(party_code: str):
-    for party in party_list:
-        if party["code"] == party_code:
-            return party
-    raise HTTPException(status_code=404, detail="Party not found")
+@app.get("/rooms/code/{room_code}")
+def get_room_from_code(room_code: str):
+    for room in room_list:
+        if room["code"] == room_code:
+            return room
+    raise HTTPException(status_code=404, detail="Room not found")
 
 #guest
 @app.post("/guests/join")
 def guest_join(guest_data: GuestJoin):
     
-    for party in party_list:
-        if guest_data.party_code == party["code"]:
+    for room in room_list:
+        if guest_data.room_code == room["code"]:
             guest_id = generate_random_code(6)
             
             guest = {
                 "name": guest_data.name,
                 "guest_id": guest_id,
-                "party_id": party["id"]
+                "room_id": room["id"]
                 }
             
             guest_list.append(guest)
             return guest
             
         
-    raise HTTPException(status_code=404, detail="Party not found")
+    raise HTTPException(status_code=404, detail="Room not found")
+        
+@app.get("/rooms/{room_id}/guests")
+def get_guests(room_id: str):
+    this_guest_list = []
+    for guest in guest_list:
+        if guest["room_id"] == room_id:
+            this_guest_list.append(guest)
+    return this_guest_list
+        
+
+        
+            
+        
         
